0e59f7aacc57edb488b0903af0c0c2b709002a59
Akien MacIain <akienm@gmail.com>
2015-01-10 16:28:32 -0800

    copied from A1. Going to try some larger scale simplifications on this one.

--- logging/logging_a2.py
+++ logging/logging_a2.py
@@ -2,7 +2,8 @@
 
 # Puzzle #2: logging
 # Akien MacIain 12/26/2014
-# algorithm 2 - merge sort
+# algorithm 1 - Parametrised Periodic Sort
+# Note: algorithm 0 - "Sort each time 300 is exceeded" was far too slow
 
 # As a [role] I want [feature] so that [benefit]
 # Given [initial context], when [event occurs], then [ensure some outcomes]
@@ -20,83 +21,174 @@ import os.path
 import sys
 import time
 import random
+import cProfile
+from collections import deque
+import threading
+import pstats
 
 
-class Profiler():
-    profile_data = dict()
-    accum = 0
-    state = 1
-    last_started = 2
-    name = 3
+class EventProcessor(threading.Thread):
 
-    @classmethod
-    def start(cls, passed_name):
-        if passed_name in cls.profile_data:
-            data = cls.profile_data[passed_name]
-            if data[cls.state] != "started":
-                data[cls.state] = "started"
-                data[cls.last_started] = time.time()
-                cls.profile_data[passed_name] = data
-        else:
-            data = [0, "started", time.time(), passed_name]
-            cls.profile_data[passed_name] = data
+    def __init__(self, data_file_name):
+        threading.Thread.__init__(self)
+        self.output_buffer = deque()
+        self.data_file_name = data_file_name
+        self.lock = threading.Lock()
+        self.stream_initialized = False
+        self.stream_complete = False
+        if _thread_enable:
+            self.start()
 
-    @classmethod
-    def stop(cls, passed_name):
-        if passed_name in cls.profile_data:
-            data = cls.profile_data[passed_name]
-            if data[cls.state] == "started":
-                data[cls.state] = "paused"
-                data[cls.accum] = time.time() - data[cls.last_started]
-                data[cls.last_started] = 0
-                cls.profile_data[passed_name] = data
+    def event_generator(self):
+        while (not self.stream_complete) or self.output_buffer:
+            while not self.output_buffer:
+                time.sleep(0.05)
+            event = self.output_buffer.popleft()
+            yield event
 
-    @classmethod
-    def dump(cls, output_file_handle=False):
-        close_file = False
-        if not output_file_handle:
-             output_file_handle = open('profile.log', 'a')
+        yield False
 
-        biggest = 0
-        cls.tee(output_file_handle, "----------------------------------", True)
-        cls.tee(output_file_handle, "Profiling results: {0}".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())), True)
-        list_of_names = list()
-        for item in cls.profile_data.items():
-            list_of_names.append(item[1])
-        list_of_names.sort(None, None, True)
-        for item in list_of_names:
-            if item[cls.state] == "started":
-                profile(item[cls.name])   # stops that timer
-            cls.tee(output_file_handle, "  name: {0}, time: {1}".format(item[cls.name], item[cls.accum]), True)
-            if item[cls.accum] > biggest:
-                biggest = item[cls.accum]
-        cls.tee(output_file_handle, "----------------------------------", True)
+    def run(self):
+        self.process()
 
-        if close_file:
-            output_file_handle.close()
-        return biggest
+    def process(self):
 
-    @classmethod
-    def tee(cls, file_handle, data, print_flag):
-        if file_handle:
-            file_handle.write("{0}\n".format(data))
-        if print_flag:
-            print data
-# end tee
+        class Bag(object):
+            def __init__(self, **kwargs):
+                self.__dict__.update(kwargs)
+
+        class Event(object):
+            def __init__(self, time, city):
+                self.time = time
+                self.city = city
+
+            def __str__(self):
+                return "{0} {1}".format(self.time, self.city)
+
+            def __repr__(self):
+                return "{0} {1}".format(self.time, self.city)
+
+        counters = Bag(
+            most_recent=float(0),
+            least_recent=float(10000000000),
+            most_recent_printed=0,
+            count_of_printed=0,
+            read_count=0,
+            read_cycle_count=0,
+            new_target=None
+        )
+        flags = Bag(
+            spacing=float(300),
+            items_to_read_per_cycle=50000
+        )
+
+        cycling_buffer = dict()
+
+        data_file_name = self.data_file_name
+        line = None
+        city = None
+        time_of_entry = None
+
+        def prep_data():  # uses line, counters
+            sep = line.find(" ")
+            city = line[sep+1:]
+            time_of_entry = float(line[0:sep-1])
+            if time_of_entry > counters.most_recent:
+                counters.most_recent = time_of_entry
+            if time_of_entry < counters.least_recent:
+                counters.least_recent = time_of_entry
+            counters.new_target = counters.most_recent - flags.spacing
+            return time_of_entry, city
+
+        def add_entry():  # uses time_of_entry, city, cycling_buffer
+            if time_of_entry not in cycling_buffer:
+                cycling_buffer[time_of_entry] = []
+            current_event = Event(time_of_entry, city)
+            current_event.line = line
+            cycling_buffer[time_of_entry].append(current_event)
+
+        def process_one():  # uses event_key, cycling_buffer, counters
+            event_list = cycling_buffer[event_key]
+            for event in event_list:
+                counters.count_of_printed += 1
+                self.output_buffer.append(event)
+                if event.time < counters.most_recent_printed:
+                    raise Exception("sort failed event.time {0} < counters.most_recent_printed {1}".format(
+                        event.time, counters.most_recent_printed))
+                counters.most_recent_printed = event.time
+                counters.least_recent = event.time
+            del cycling_buffer[event_key]
+
+        with open(data_file_name, 'r') as input_file:
+
+            for line in input_file:
+                counters.read_count += 1
+                counters.read_cycle_count += 1
+
+                time_of_entry, city = prep_data()  # uses line, counters
+                add_entry()  # uses time_of_entry, city, cycling_buffer
+
+                if (counters.least_recent <= counters.new_target) and \
+                        (counters.read_cycle_count > flags.items_to_read_per_cycle):
+                    while self.output_buffer:
+                        time.sleep(0.05)
+                    # self.lock.acquire(True)
+                    counters.read_cycle_count = 0
+                    keys_to_test = list(cycling_buffer.keys())
+                    keys_to_test.sort(None, None, False)
+                    for event_key in keys_to_test:
+                        if cycling_buffer[event_key][0].time < counters.new_target:
+                            process_one()  # uses event_key, cycling_buffer, counters)
+                        else:
+                            break
+                    self.stream_initialized = True
+                    # self.lock.release()
 
 
-def tee(file_handle, data, print_too):
-    file_handle.write("{0}\n".format(data))
-    if print_too:
-        print data
-    for stream in (sys.stdout, sys.stderr):
-        stream.flush()
-# end tee
+        if cycling_buffer:
+            while self.output_buffer:
+                time.sleep(0.05)
+            # self.lock.acquire(True)
+            keys_to_test = list(cycling_buffer.keys())
+            keys_to_test.sort()
+            for event_key in keys_to_test:
+                process_one()  # event_key, cycling_buffer, counters)
+            # self.lock.release()
+
+        self.stream_initialized = True
+        self.stream_complete = True
+# end EventProcessor
+
+
+_event_processors = dict()
+
+
+def event_stream(data_file_name):
+    global _event_processors
+    if data_file_name not in _event_processors:
+        _event_processors[data_file_name] = EventProcessor(data_file_name)
+        if not _thread_enable:
+            _event_processors[data_file_name].process()
+    generator = _event_processors[data_file_name].event_generator()
+    return generator
+# end event_stream
+
+output_handle = None
+
+
+def update_model(event):
+    global output_handle
+    if not output_handle:
+        output_handle = open("result.txt", 'w')
+    if event:
+        # output_handle.write(event.__str__())
+        output_handle.write(event.line)
+    else:
+        output_handle.close()
 
 
 def create_sample_file(write_file_name):
     """ creates sample data file """
-    Profiler.start("sample create")
     JITTER = 275
     TICKS = 1000
     LINES_PER_TICK = 1000
@@ -113,22 +205,15 @@ def create_sample_file(write_file_name):
         for tick in xrange(TICKS):
             now = start + tick
             for num_line in xrange(LINES_PER_TICK):
-                fh.write(log_line(now)+"\n")
+                fh.write(log_line(now) + "\n")
 
-    Profiler.stop("sample create")
 # end create_sample_file
 
 
-class Event:
-    def __init__(self):
-        self.time = 0.0
-        self.city = ""
-
-
 def main():
-
+    start = time.time()
     if len(sys.argv) < 2 or (len(sys.argv) > 1 and (not os.path.isfile(sys.argv[1]))):
-        # we have to create file
+        # if we got here, we have to create the input file
         data_file_name = "data.txt"
         if not os.path.isfile(data_file_name):
             create_sample_file(data_file_name)
@@ -140,92 +225,28 @@ def main():
     # data_file_name = "data.txt"
     # create_sample_file(data_file_name)
 
-    cycling_buffer = dict()
-    most_recent = float(0)
-    least_recent = float(10000000000)
-    most_recent_printed = 0
-    count_of_printed = 0
-    read_count = 0
-    read_cycle_count = 0
+    # generator = event_stream(data_file_name)
+    for event in event_stream(data_file_name):
+        update_model(event)
+        print event
 
-    spacing = float(300)
-    items_to_read_per_cycle = 50000
-    stop_at = -1
-    debug_halt = False
-
-    print "cycle count: {0} width in seconds: {1}".format(items_to_read_per_cycle, spacing)
-
-    start_read = time.time()
-    Profiler.start("overall")
-    with open("result.txt", 'w') as output:
-        with open(data_file_name, 'r') as fh:
-
-            Profiler.start("everything_else")  # starts
-            for line in fh:
-                read_count += 1
-                read_cycle_count += 1
-                if read_count > stop_at and stop_at != -1:
-                    debug_halt = True
-                if debug_halt:
-                    break
-
-                city = line[line.find(" "):].strip()
-                time_of_entry = line[0:line.find(" ")]
-                time_of_entry_numeric = float(time_of_entry)
-                if time_of_entry_numeric > most_recent:
-                    most_recent = time_of_entry_numeric
-                if time_of_entry_numeric < least_recent:
-                    least_recent = time_of_entry_numeric
-
-                cycling_buffer[time_of_entry] = city
-                new_target = most_recent - spacing
-                if least_recent <= new_target and read_cycle_count > items_to_read_per_cycle:
-                    read_cycle_count = 0
-                    Profiler.stop("everything_else")
-                    Profiler.start("make_list")
-                    keys_to_test = list(cycling_buffer.keys())
-                    Profiler.stop("make_list")
-                    Profiler.start("sort")
-                    keys_to_test.sort(None, None, False)
-                    Profiler.stop("sort")
-                    Profiler.start("everything_else")
-                    for item in keys_to_test:
-                        item_numeric = float(item)
-                        if item_numeric < new_target:
-                            count_of_printed += 1
-                            message = "time:{0} city:{1} output#:{2} time:{3} #inqueue:{4} queuesize: {5}".format(item, cycling_buffer[item], count_of_printed, time.time() - start_read, len(cycling_buffer), most_recent - least_recent)
-                            tee(output, message, False)
-                            if item_numeric < most_recent_printed:
-                                raise Exception("sort failed")
-                            most_recent_printed = item_numeric
-                            least_recent = item_numeric
-                            del cycling_buffer[item]
-                        else:
-                            break
-
-
-        Profiler.start("flush")
-        Profiler.start("make_list")
-        keys_to_test = list(cycling_buffer.keys())
-        print "keys now: {0}".format(len(keys_to_test))
-        print "width in seconds: {0}".format(most_recent - least_recent)
-        Profiler.stop("make_list")
-        Profiler.start("sort")
-        keys_to_test.sort()
-        Profiler.stop("sort")
-        Profiler.start("everything_else")
-        for item in keys_to_test:
-            count_of_printed += 1
-            message = "time:{0} city:{1} output#:{2} time:{3} #inqueue:{4} queuesize: {5}".format(item, cycling_buffer[item], count_of_printed, time.time() - start_read, len(cycling_buffer), most_recent - least_recent)
-            tee(output, message, False)
-            del cycling_buffer[item]
-        Profiler.stop("everything_else")
-        Profiler.stop("flush")
-
-        Profiler.stop("overall")
-
-        Profiler.dump()
+    end = time.time()
+    print end - start
 # end main
 
+_thread_enable = True
+_profile_and_debug = True
 
-main()
+
+def other():
+    cProfile.run('main()', "profiler.raw", "tottime")
+    output_handle = open("profiler.log", 'a')
+    p = pstats.Stats('profiler.raw', stream=output_handle)
+    p.strip_dirs().sort_stats("tottime").print_stats(12)
+    output_handle.close()
+
+if _profile_and_debug:
+    other()
+else:
+    main()
+
